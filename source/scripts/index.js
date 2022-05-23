85416eab19c6589ab0f1eb30fa832dad16a11fe3
Yuri Litvinov <uslitvinov@53xapps.com>
2022-05-24 00:04:44 +0500

    header add all

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,27 @@
-const menuButton = document.querySelectorAll('.footer__span');
-const menu = document.querySelectorAll('.footer__menu');
+const menuButton = document.querySelector('.header__button-nav');
+const menu = document.querySelector('.header__themes--noactive');
+
+menuButton.addEventListener('click', () => {
+	menu.classList.toggle('header__themes');
+	menuButton.classList.toggle('rotate');
+});
 
 
+const contentButton = document.querySelector('.content__nav-button');
+const contentMenu = document.querySelector('.content__menu');
+
+contentButton.addEventListener('click', () => {
+	contentMenu.classList.toggle('content__menu--active');
+
+});
 
 
+// const btnSearch = document.querySelector('.user-nav__search');
+// const searchForm = document.querySelector('.user-nav__noactive');
+// const searchInput = document.querySelector('.user-nav__input');
+
+// btnSearch.addEventListener('click', () => {
+// 	searchForm.classList.toggle('user-nav--active');
+// 	searchInput.classList.toggle('user-nav--active');
+// 	searchInput.focus();
+// });
