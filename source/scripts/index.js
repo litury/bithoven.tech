4cac4716c51b88419d4c976aa7145e2c6b023ea8
Yuri Litvinov <uslitvinov@53xapps.com>
2022-05-26 16:05:10 +0500

    header js fix

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,53 +1,103 @@
-// const menuButton = document.querySelector('.header__button-nav');
-// const menu = document.querySelector('.header__themes--noactive');
+//шапка поиска
 
-// menuButton.addEventListener('click', () => {
-// 	menu.classList.toggle('header__themes');
-// 	menuButton.classList.toggle('rotate');
+const logoBtnMobMenu = document.querySelector('.header-mobile__logotype');
+const menuBtnMob = document.querySelector('.header__button-deck');
+const langBtnMob = document.querySelector('.header-mobile__lang');
+const searchBtnMob = document.querySelector('.header-mobile__search');
+const authhBtnMob = document.querySelector('.header-mobile__auth');
+
+
+logoBtnMobMenu.addEventListener('click', () => {
+	menuBtnMob.classList.toggle('header__button--no');
+	langBtnMob.classList.toggle('header__button--no');
+
+	searchBtnMob.classList.toggle('header__button--yes');
+	authhBtnMob.classList.toggle('header__button--yes');
+
+});
+
+// menuBtnMob.addEventListener('click', () => {
+// 	menuBtnMob.classList.toggle('header__button--no');
+// 	langBtnMob.classList.toggle('header__button--no');
+
+// 	searchBtnMob.classList.toggle('header__button--yes');
+// 	authhBtnMob.classList.toggle('header__button--yes');
+
 // });
 
 
-// Строка поиска
-const menuButton = document.querySelector('.btn');
-const inputDeckt = document.querySelector('.input');
+
+
+//header mobile
+const headBtnMobMenu = document.querySelector('.header__button_type_menu');
+const headBtnMobOF = document.querySelector('.header__button-onof');
+
+const headMobMenu = document.querySelector('.header-mobile');
+const headMobMenuBlock = document.querySelector('.header__menu-wrap');
+
+
+headBtnMobMenu.addEventListener('click', () => {
+	headMobMenu.classList.toggle('header__button--no');
+	headMobMenuBlock.classList.toggle('header__button--yes');
+});
+
+headBtnMobOF.addEventListener('click', () => {
+	headMobMenu.classList.toggle('header__button--no');
+	headMobMenuBlock.classList.toggle('header__button--yes');
+});
+
+// подменю в мобильном меню
+const menuButton = document.querySelector('.header__button-nav');
+const menu = document.querySelector('.header__themes--noactive');
 
 menuButton.addEventListener('click', () => {
-	inputDeckt.classList.toggle('inclicked');
-	menuButton.classList.toggle('close');
-
+	menu.classList.toggle('header__themes');
+	menuButton.classList.toggle('rotate');
 });
 
 
-const menuButtonMob = document.querySelector('.btn_media_mobile');
-const inputMobile = document.querySelector('.input_media_mobile');
-const labelMobile = document.querySelector('.middle__label_media_mobile');
-
-const homeMob = document.querySelector('.header__button_type_home');
-const profiMob = document.querySelector('.header__button_type_profile');
-const menuMob = document.querySelector('.header__button_type_menu');
 
 
-menuButtonMob.addEventListener('click', () => {
-	inputMobile.classList.toggle('inclicked');
-	menuButtonMob.classList.toggle('close');
-	labelMobile.classList.toggle('middle__label_media_mobilenone');
-	homeMob.classList.toggle('middle__label_media_mobilenone');
-	profiMob.classList.toggle('middle__label_media_mobilenone');
-	menuMob.classList.toggle('middle__label_media_mobilenone');
-});
+// // Строка поиска
+// const menuButton = document.querySelector('.btn');
+// const inputDeckt = document.querySelector('.input');
+
+// menuButton.addEventListener('click', () => {
+// 	inputDeckt.classList.toggle('inclicked');
+// 	menuButton.classList.toggle('close');
+
+// });
 
 
-const headerAccountBtn = document.querySelector('.header__button-acc');
-const headerAccount = document.querySelector('.header__svg-login');
-const headerAccImgBtn = document.querySelector('.header__button-login');
+// const menuButtonMob = document.querySelector('.btn_media_mobile');
+// const inputMobile = document.querySelector('.input_media_mobile');
+// const labelMobile = document.querySelector('.middle__label_media_mobile');
+
+// const homeMob = document.querySelector('.header__button_type_home');
+// const profiMob = document.querySelector('.header__button_type_profile');
+// const menuMob = document.querySelector('.header__button_type_menu');
 
 
+// menuButtonMob.addEventListener('click', () => {
+// 	inputMobile.classList.toggle('inclicked');
+// 	menuButtonMob.classList.toggle('close');
+// 	labelMobile.classList.toggle('middle__label_media_mobilenone');
+// 	homeMob.classList.toggle('middle__label_media_mobilenone');
+// 	profiMob.classList.toggle('middle__label_media_mobilenone');
+// 	menuMob.classList.toggle('middle__label_media_mobilenone');
+// });
 
-headerAccountBtn.addEventListener('click', () => {
-	headerAccount.classList.toggle('header__button--no');
-	headerAccImgBtn.classList.toggle('header__button--yes');
 
-});
+// const headerAccountBtn = document.querySelector('.header__button-acc');
+// const headerAccount = document.querySelector('.header__svg-login');
+// const headerAccImgBtn = document.querySelector('.header__button-login');
+
+// headerAccountBtn.addEventListener('click', () => {
+// 	headerAccount.classList.toggle('header__button--no');
+// 	headerAccImgBtn.classList.toggle('header__button--yes');
+// });
+
+
 
 
 
