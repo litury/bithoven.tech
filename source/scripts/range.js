deb187b4a72549c901b41a4a22d2f062b952aed2
Yuri Litvinov <uslitvinov@53xapps.com>
2022-06-07 23:16:04 +0500

    range desktop end

--- source/scripts/range.js
+++ source/scripts/range.js
@@ -1,10 +1,27 @@
-var rangeSlider = document.getElementById('range-slider');
+var nonLinearSlider = document.getElementById('nonlinear');
 
-noUiSlider.create(rangeSlider, {
-	start: [20, 80],
+noUiSlider.create(nonLinearSlider, {
 	connect: true,
+	snap: true,
+	behaviour: 'tap',
+	start: [1, 100],
 	range: {
-		'min': 0,
-		'max': 100
+		'min': 1,
+		'10%': 3,
+		'16,67%': 5,
+		'33,33%': 10,
+		'50%': 15,
+		'66,67%': 20,
+		'max': 30
 	}
+});
+
+
+var snapValues = [
+	document.getElementById('slider-snap-value-lower'),
+	document.getElementById('slider-snap-value-upper')
+];
+
+snapSlider.noUiSlider.on('update', function (values, handle) {
+	snapValues[handle].innerHTML = values[handle];
 });
