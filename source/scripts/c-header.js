6b4346626d780f11590883803c93c56220d45110
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-06-07 15:32:52 +0300

    Add mobile-header

--- source/scripts/c-header.js
+++ source/scripts/c-header.js
@@ -18,22 +18,57 @@
 		}
 	});
 
+	// hide scroll
+
+	const hideBodyScroll = () => body.classList.toggle("page__body_state_no-scroll");
+
 	// mobile search
 	
 	const mobileSearch = document.querySelector(".c-mobile-search");
 	const mobileSearchInput = mobileSearch.querySelector(".c-mobile-search__input");
 	const mobileSearchClose = mobileSearch.querySelector(".c-mobile-search__close");
 
+	const toggleMobileSearch = () => {
+		mobileSearchInput.focus();
+
+		mobileSearchClose.addEventListener("click", () => {
+			mobileSearch.classList.remove("c-mobile-search_state_active");
+		});
+	};
+
 	new toggleElement(
 		document.querySelector(".c-mobile-header__button_type_search"),
 		mobileSearch,
 		"c-mobile-search_state_active",
-		() => {
-			mobileSearchInput.focus();
+		toggleMobileSearch,
+	).init();
 
-			mobileSearchClose.addEventListener("click", () => {
-				mobileSearch.classList.remove("c-mobile-search_state_active");
-			});
-		}
+	new toggleElement(
+		document.querySelector(".c-menu__button_type_search"),
+		mobileSearch,
+		"c-mobile-search_state_active",
+		toggleMobileSearch,
+	).init();
+
+	// mobile menu
+
+	const toggleMobileMenu = (event) => {
+		hideBodyScroll();
+
+		document.querySelector(".c-menu__button_type_menu").classList.toggle("c-menu__button_state_active");
+
+		document.querySelector(".c-mobile-header-menu__close").addEventListener("click", () => {
+
+			hideBodyScroll();
+
+			document.querySelector(".c-mobile-header-menu").classList.remove("c-mobile-header-menu_state_active");
+		});
+	};
+
+	new toggleElement(
+		document.querySelector(".c-menu__button_type_menu"),
+		document.querySelector(".c-mobile-header-menu"),
+		"c-mobile-header-menu_state_active",
+		toggleMobileMenu
 	).init();
 })();
