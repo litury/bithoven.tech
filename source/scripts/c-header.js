1917a26c8f6f7308c62abbd67a9f4174ecb190f2
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-06-08 12:04:13 +0300

    Update mobile header

--- source/scripts/c-header.js
+++ source/scripts/c-header.js
@@ -18,6 +18,12 @@
 		}
 	});
 
+	// close block
+	const closeBlock = (block, element) => element.addEventListener("click", () => {
+		document.querySelectorAll(".c-menu__button").forEach(button => button.classList.remove("c-menu__button_state_active"));
+		block.classList.remove(block.getAttribute("data-menu-class"))
+	});
+
 	// hide scroll
 
 	const hideBodyScroll = () => body.classList.toggle("page__body_state_no-scroll");
@@ -32,6 +38,7 @@
 		mobileSearchInput.focus();
 
 		mobileSearchClose.addEventListener("click", () => {
+			document.querySelector(".c-menu__button_type_search").classList.remove("c-menu__button_state_active");
 			mobileSearch.classList.remove("c-mobile-search_state_active");
 		});
 	};
@@ -52,17 +59,12 @@
 
 	// mobile menu
 
-	const toggleMobileMenu = (event) => {
+	const toggleMobileMenu = (className) => {
 		hideBodyScroll();
-
-		document.querySelector(".c-menu__button_type_menu").classList.toggle("c-menu__button_state_active");
-
-		document.querySelector(".c-mobile-header-menu__close").addEventListener("click", () => {
-
-			hideBodyScroll();
-
-			document.querySelector(".c-mobile-header-menu").classList.remove("c-mobile-header-menu_state_active");
-		});
+		closeBlock(
+			className,
+			document.querySelector(".c-mobile-header-menu__close")
+		);
 	};
 
 	new toggleElement(
@@ -70,5 +72,20 @@
 		document.querySelector(".c-mobile-header-menu"),
 		"c-mobile-header-menu_state_active",
 		toggleMobileMenu
+	).init();
+
+	// auth
+	const menuAuth = document.querySelector(".c-menu__button_type_auth");
+	const blockAuth = document.querySelector(".c-mobile-header-auth");
+	const blockAuthClose = blockAuth.querySelector(".c-mobile-header-auth__close");
+	const toggleAuth = (element) => {
+		closeBlock(element, blockAuthClose);
+	};
+
+	new toggleElement(
+		menuAuth,
+		blockAuth,
+		"c-mobile-header-auth_state_active",
+		toggleAuth,
 	).init();
 })();
---
+++ source/scripts/c-menu.js
@@ -0,0 +1,13 @@
+;(() => {
+	"use strict";
+
+	const mobileMenuButton = document.querySelector(".c-mobile-header-menu__button");
+	const mobileMenuThemes = document.querySelector(".c-mobile-header-menu__themes");
+
+	mobileMenuButton.addEventListener("click", (event) => {
+		const currentButton = event.currentTarget;
+
+		currentButton.classList.toggle("c-mobile-header-menu__button_state_active");
+		mobileMenuThemes.classList.toggle("c-mobile-header-menu__themes_state_active");
+	});
+})();
