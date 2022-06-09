d619825375c7eefae6165c014cf3ad9ad560b20d
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-06-09 13:28:20 +0300

    Fix 124

---
+++ source/scripts/profile.js
@@ -0,0 +1,29 @@
+;(() => {
+	"use strict";
+
+	const profile = document.querySelector(".profile");
+	const profileEdit = profile.querySelector(".profile__edit");
+	const profileNickname = profile.querySelector(".profile__nickname");
+	const profileInput = profile.querySelector(".profile__input");
+	const profileBlock = document.querySelector(".profile__block");
+
+	const editNickname = () => {
+		if (!profileNickname.hidden) {
+			profileNickname.hidden = true;
+			profileInput.hidden = false;
+			profileInput.value = profileNickname.textContent.trim();
+			profileInput.focus();
+		}
+	};
+
+	const saveNickname = (event) => {
+		event.preventDefault();
+	
+		profileNickname.textContent = profileInput.value;
+		profileNickname.hidden = false;
+		profileInput.hidden = true;
+	};
+
+	profileEdit.addEventListener("click", editNickname);
+	profileBlock.addEventListener("submit", saveNickname);
+})();
