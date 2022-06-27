b48287afc392c8531cde91ea9f8a99da9e35bdd1
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-06-27 11:54:25 +0300

    Fix 181

--- source/scripts/article.js
+++ source/scripts/article.js
@@ -7,8 +7,33 @@
 	const buttonReply = document.querySelector(".content__button-reply");
 	const contentSuccess = document.querySelector(".content__success");
 
+	function copyToClipboard(text) {
+		if (window.clipboardData && window.clipboardData.setData) {
+				// Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+				return window.clipboardData.setData("Text", text);
+
+		}
+		else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+				var textarea = document.createElement("textarea");
+				textarea.textContent = text;
+				textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+				document.body.appendChild(textarea);
+				textarea.select();
+				try {
+						return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+				}
+				catch (ex) {
+						console.warn("Copy to clipboard failed.", ex);
+						return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+				}
+				finally {
+						document.body.removeChild(textarea);
+				}
+		}
+}
+
 	buttonReply.addEventListener("click", () => {
-		navigator.clipboard.writeText(window.location.href);
+		copyToClipboard(window.location.href);
 		contentSuccess.classList.add("content__success_state_active");
 		setTimeout(() => {
 			contentSuccess.classList.remove("content__success_state_active");
