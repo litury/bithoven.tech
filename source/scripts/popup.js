13e5945cc9e5121e608e643bdae66cc5859cce19
Yuri Litvinov <uslitvinov@53xapps.com>
2022-06-27 20:10:26 +0500

    popup fix

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,25 +1,71 @@
-
-var modal = document.getElementById('myModal');
-
-
-var btn = document.getElementById("myBtn");
-
-
-var span = document.getElementsByClassName("popup__close")[0];
+;
+(() => {
+	"use strict";
+	// var modal = document.getElementById('myModal');
 
 
-btn.onclick = function () {
-	modal.style.display = "block";
-}
+	// var btn = document.getElementById("myBtn");
 
 
-span.onclick = function () {
-	modal.style.display = "none";
-}
+	// var span = document.getElementsByClassName("close")[0];
 
 
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-}
+	// btn.onclick = function () {
+	// 	modal.style.display = "block";
+	// }
+
+
+
+
+
+	// window.onclick = function (event) {
+	// 	if (event.target == modal) {
+	// 		modal.style.display = "none";
+	// 	}
+	// }
+
+
+	const btns = document.querySelectorAll('.popup-button');
+	const modalOverlay = document.querySelector('.modal-overlay');
+	const modals = document.querySelectorAll('.modal');
+	const span = document.querySelectorAll(".popup__close");
+
+
+	span.forEach((el) => {
+		el.addEventListener('click', (e) => {
+
+			modalOverlay.forEach((el) => {
+				el.classList.add('modal--nonvisible');
+			});
+
+
+		});
+	});
+
+
+
+	btns.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			let path = e.currentTarget.getAttribute('data-path');
+
+			modals.forEach((el) => {
+				el.classList.remove('modal--visible');
+			});
+
+			document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+			modalOverlay.classList.add('modal-overlay--visible');
+		});
+	});
+
+	modalOverlay.addEventListener('click', (e) => {
+		console.log(e.target);
+
+		if (e.target == modalOverlay) {
+			modalOverlay.classList.remove('modal-overlay--visible');
+			modals.forEach((el) => {
+				el.classList.remove('modal--visible');
+			});
+		}
+	});
+
+})();
