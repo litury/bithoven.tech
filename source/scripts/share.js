c9a26c9aad8198235f726b121e6a316997cafaf4
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-06-29 10:56:19 +0300

    Fix 231

---
+++ source/scripts/share.js
@@ -0,0 +1,21 @@
+;(() => {
+	"use strict";
+
+	const currentLink = location.href;
+	const shareVK =
+		document.querySelector(".content__share_social_vk");
+	const shareTelegram =
+		document.querySelector(".content__share_social_telegram");
+	const shareTwitter =
+		document.querySelector(".content__share_social_twitter");
+	const shareLinkedin =
+		document.querySelector(".content__share_social_linkedin");
+
+	const setLink = (element, link) =>
+		element.setAttribute("href", `${link}?url=${currentLink}`);
+
+	setLink(shareVK, "https://vk.com/share.php");
+	setLink(shareTelegram, "https://telegram.me/share/url");
+	setLink(shareTwitter, "https://www.twitter.com/share");
+	setLink(shareLinkedin, "https://www.linkedin.com/sharing/share-offsite/");
+})();
