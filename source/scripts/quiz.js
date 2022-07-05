6a02f78a682fd85f8ae1c9f193e63a6a4bfee814
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-07-05 16:47:35 +0300

    Fix quiz

--- source/scripts/quiz.js
+++ source/scripts/quiz.js
@@ -3,7 +3,6 @@
 
 	const quizBlock = document.querySelector(".page__quiz-card_type_primary");
 	const quizAuth = document.querySelector(".quiz__auth");
-	const quizBlockStart = document.querySelector(".page__quiz-card_type_start");
 	const quizProgressBar = document.querySelector(".page__quiz-card_type_primary .quiz__progress-bar");
 	const quizBlocks = document.querySelectorAll(".page__quiz-card_type_primary .quiz__block");
 	const quizButtons = document.querySelectorAll(".page__quiz-card_type_primary .quiz__main-button");
@@ -19,12 +18,14 @@
 
 	localStorage.setItem("correct-answer", JSON.stringify(0));
 
-	quizBlocks.forEach((element, index, array) => {
-		const quizStep = document.createElement("span");
-		quizStep.classList.add("quiz__progress-bar-step");
+	if (quizBlocks) {
+		quizBlocks.forEach((element, index, array) => {
+			const quizStep = document.createElement("span");
+			quizStep.classList.add("quiz__progress-bar-step");
 
-		quizProgressBar.appendChild(quizStep);
-	});
+			quizProgressBar.appendChild(quizStep);
+		});
+	}
 
 	if (quizAuth) {
 		quizAuth.addEventListener("click", () => {
@@ -32,41 +33,46 @@
 		});
 	}
 
-	quizCatalogMainButton.addEventListener("click", (event) => {
-		const currentButton = event.currentTarget;
-		const parent = currentButton.closest(".quiz");
-		quizBlock.hidden = false;
-		parent.hidden = true;
-	});
+	if (quizCatalogMainButton) {
+		quizCatalogMainButton.addEventListener("click", (event) => {
+			const currentButton = event.currentTarget;
+			const parent = currentButton.closest(".quiz");
+			quizBlock.hidden = false;
+			parent.hidden = true;
+		});
+	}
 
-	quizButtons.forEach(quizeButton => quizeButton.addEventListener("click", (event) => {
-		const currentButton = event.currentTarget;
-		const currentButtonValue = currentButton.getAttribute("data-quiz-answer");
+	if (quizButtons) {
+		quizButtons.forEach(quizeButton => quizeButton.addEventListener("click", (event) => {
+			const currentButton = event.currentTarget;
+			const currentButtonValue = currentButton.getAttribute("data-quiz-answer");
 
-		const currentQuizBlock = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer");
+			const currentQuizBlock = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer");
 
-		if (currentButtonValue === currentQuizBlock) {
-			quizAnswer.classList.remove("quiz__answer_state_hidden");
-			quizAnswer.classList.remove("quiz__answer_state_error");
-			quizAnswerTitle.textContent = "Правильно";
-			quizAnswerParagraph.textContent = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer-paragraph");
-			quizButtons.forEach(button => button.classList.add("quiz__main-button_state_hidden"));
-			quizNext.classList.remove("quiz__next_state_hidden");
-			quizNext.style.setProperty("--background", "#27ae60");
-			let correctAnswer = JSON.parse(localStorage.getItem("correct-answer"));
-			correctAnswer++;
-			localStorage.setItem("correct-answer", JSON.parse(correctAnswer));
-		} else {
-			quizAnswer.classList.remove("quiz__answer_state_hidden");
-			quizAnswer.classList.add("quiz__answer_state_error");
-			quizAnswerParagraph.textContent = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer-paragraph");
-			quizAnswerTitle.textContent = "Не правильно";
-			quizButtons.forEach(button => button.classList.add("quiz__main-button_state_hidden"));
-			quizNext.classList.remove("quiz__next_state_hidden");
-			quizNext.style.setProperty("--background", "#d92b4d");
-		}
-	}));
+			if (currentButtonValue === currentQuizBlock) {
+				quizAnswer.classList.remove("quiz__answer_state_hidden");
+				quizAnswer.classList.remove("quiz__answer_state_error");
+				quizAnswerTitle.textContent = "Правильно";
+				quizAnswerParagraph.textContent = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer-paragraph");
+				quizButtons.forEach(button => button.classList.add("quiz__main-button_state_hidden"));
+				quizNext.classList.remove("quiz__next_state_hidden");
+				quizNext.style.setProperty("--background", "#27ae60");
+				let correctAnswer = JSON.parse(localStorage.getItem("correct-answer"));
+				correctAnswer++;
+				localStorage.setItem("correct-answer", JSON.parse(correctAnswer));
+			} else {
+				quizAnswer.classList.remove("quiz__answer_state_hidden");
+				quizAnswer.classList.add("quiz__answer_state_error");
+				quizAnswerParagraph.textContent = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]").getAttribute("data-quiz-answer-paragraph");
+				quizAnswerTitle.textContent = "Не правильно";
+				quizButtons.forEach(button => button.classList.add("quiz__main-button_state_hidden"));
+				quizNext.classList.remove("quiz__next_state_hidden");
+				quizNext.style.setProperty("--background", "#d92b4d");
+			}
+		}));
+	}
 
+	if (quizNext) {
 	quizNext.addEventListener("click", (event) => {
 		const currentBlock = document.querySelector(".page__quiz-card_type_primary [data-quiz-current]");
 		const nextBlock = currentBlock.nextElementSibling;
@@ -123,18 +129,23 @@
 		quizButtons.forEach(button => button.classList.remove("quiz__main-button_state_hidden"));
 		quizAnswer.classList.add("quiz__answer_state_hidden");
 	});
+	}
 
-	quizRestart.addEventListener("click", () => {
-		location.reload();
-	});
+	if (quizRestart) {
+		quizRestart.addEventListener("click", () => {
+			location.reload();
+		});
+	}
+	
+	if (quizReply) {
+		quizReply.addEventListener("click", () => {
+			navigator.clipboard.writeText(window.location.href);
+			const quizSuccess = document.querySelector(".quiz__success");
+			quizSuccess.classList.add("quiz__success_state_active");
 
-	quizReply.addEventListener("click", () => {
-		navigator.clipboard.writeText(window.location.href);
-		const quizSuccess = document.querySelector(".quiz__success");
-		quizSuccess.classList.add("quiz__success_state_active");
-
-		setTimeout(() => {
-			quizSuccess.classList.remove("quiz__succes_state_active");
-		}, 1000);
-	});
+			setTimeout(() => {
+				quizSuccess.classList.remove("quiz__succes_state_active");
+			}, 1000);
+		});
+	}
 })();
