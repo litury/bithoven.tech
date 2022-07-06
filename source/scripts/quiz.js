fa40ad7a58578ca9d130d5703d9ed1e8cba6dafd
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-07-06 17:42:16 +0300

    Fix 235

--- source/scripts/quiz.js
+++ source/scripts/quiz.js
@@ -23,6 +23,31 @@
 	const quizReply = document.querySelector(".quiz__result-reply");
 	const quizCatalogMainButton = document.querySelector(".quiz__catalog-main-button");
 
+  function copyToClipboard(text) {
+		if (window.clipboardData && window.clipboardData.setData) {
+				// Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+				return window.clipboardData.setData("Text", text);
+
+		}
+		else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+				var textarea = document.createElement("textarea");
+				textarea.textContent = text;
+				textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+				document.body.appendChild(textarea);
+				textarea.select();
+				try {
+						return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+				}
+				catch (ex) {
+						console.warn("Copy to clipboard failed.", ex);
+						return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+				}
+					finally {
+							document.body.removeChild(textarea);
+					}
+			}
+	}
+
 	localStorage.setItem("correct-answer", JSON.stringify(0));
 
 	if (quizBlocks) {
@@ -149,7 +174,7 @@
 	
 	if (quizReply) {
 		quizReply.addEventListener("click", () => {
-			navigator.clipboard.writeText(window.location.href);
+			copyToClipboard(window.location.href);
 			const quizSuccess = document.querySelector(".quiz__success");
 			quizSuccess.classList.add("quiz__success_state_active");
 
