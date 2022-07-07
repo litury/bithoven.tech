9df308d8b5895c8fe6358e4952c3e4a5e5338dd4
Harutyun Karapetyan <in.kharutyun@gmail.com>
2022-07-07 11:39:13 +0300

    Refix 281

--- source/scripts/education.js
+++ source/scripts/education.js
@@ -1,7 +1,55 @@
 ;(() => {
 	"use strict";
 
-	const paginationLinks = document.querySelectorAll(".education__menu a");
+	const paginationLinks =
+		document.querySelectorAll(".education__menu a");
+	const lastPaginationLink =
+		paginationLinks[paginationLinks.length - 1].textContent.trim();
+	const prevPage =
+		document.querySelector(".education__menu-button_prev");
+	const nextPage =
+		document.querySelector(".education__menu-button_next");
+
+	function findGetParameter(parameterName) {
+		let result = null,
+				tmp = [];
+
+		location.search
+				.substr(1)
+				.split("&")
+				.forEach(function (item) {
+					tmp = item.split("=");
+					if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+				});
+		return result;
+	}
+
+	if (findGetParameter("page") === "1") {
+		prevPage.classList.add("education__menu-button_state_hidden");
+	}
+
+	if (findGetParameter("page") === lastPaginationLink) {
+		nextPage.classList.add("education__menu-button_state_hidden");
+	}
+
+	function redirectPage(pageNumber) {
+		const currentURL = new URL(location.href);
+		const params = new URLSearchParams(currentURL.search);
+
+		params.set("page", pageNumber);
+		location.href = `education?${params.toString()}`;
+	}
+
+	prevPage.addEventListener("click", () => {
+		let currentPageNumber = +findGetParameter("page");
+		redirectPage(--currentPageNumber);
+	});
+
+
+	nextPage.addEventListener("click", () => {
+		let currentPageNumber = +findGetParameter("page");
+		redirectPage(++currentPageNumber);
+	});
 
 	paginationLinks.forEach(link => link.addEventListener("click", (event) => {
 		event.preventDefault();
@@ -9,10 +57,6 @@
 		const currentLink = event.currentTarget;
 		const pageNumber = +currentLink.textContent.trim();
 	
-		const currentURL = new URL(location.href);
-		const params = new URLSearchParams(currentURL.search);
-
-		params.set("page", pageNumber);
-		location.href = `education?${params.toString()}`;
+		redirectPage(pageNumber);
 	}));
 })();
